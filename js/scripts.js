7f05d9935c64ab7824907a102ac25eea221c388e
odetteahishakiye <ahiode6@gmail.com>
2019-07-19 21:28:58 +0200

    Change function structure

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,34 @@
-var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-var daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-var gender = document.getElementsByName("gender").value;
-
-var calend = document.getElementById('bday').value;
-var birthday = calend.split("-");
-
-var year=parseInt(birthday[0]);
-var month=parseInt(birthday[1]);
-var day= parseInt(birthday[2]);
-
-var cc = Math.round((year-1)/(100+1));
-var bornDay = Math.round( ( ( (CC/4) -2*CC-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day ) % 7);
 
 
-var changeLabel = function () {
-    for(var i=1; i<=daysOfWeek.length; i++)
-    {
-        if((bornDay==i)&&(gender=="male")){
-            result="You were born on "+daysOfWeek[i-1]+". So, your name should be "+maleNames[i-1];  
-        }
-        else if((bornDay==i)&&(gender=="female")){
-            result="You were born on "+daysOfWeek[i-1]+". So, your name should be "+femaleNames[i-1];
-        }
+
+function giveName() {
+    var calend = document.getElementById('bday').value;
+    var birthday = calend.split("-");
+
+    var year = parseFloat(birthday[0]);
+    var month = parseFloat(birthday[1]);
+    var day = parseFloat(birthday[2]);
+    var gender = document.querySelector("input[name='gender']:checked").value;
+
+    var cc = (year - 1) / 100 + 1;
+    var bornDay = Math.round((((cc / 4) - 2 * cc - 1) + ((5 * year / 4)) + ((26 * (month + 1) / 10)) + day) % 7);
+    console.log(cc);
+
+    var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    var daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    if (gender === "male") {
+        console.log("You were born on " + daysOfWeek[bornDay - 1] + ". So, your name should be " + maleNames[bornDay - 1]);
+        document.getElementById("display").innerHTML = "You were born on " + daysOfWeek[i - 1] + ". So, your name should be " + maleNames[i - 1];
     }
+    else if (gender === "female") {
+        console.log("You were born on " + daysOfWeek[bornDay - 1] + ". So, your name should be " + femaleNames[bornDay - 1]);
+        document.getElementById("display").innerHTML = "You were born on " + daysOfWeek[i - 1] + ". So, your name should be " + femaleNames[i - 1];
+    }
+    else {
+        console.log("Invalid input");
+        document.getElementById("display").innerHTML = "Invalid input";
+    }
+
 }
 
