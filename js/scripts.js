5da58c2bb1dd069d58b3fb6b87aa0bcfe183034a
odetteahishakiye <ahiode6@gmail.com>
2019-07-19 16:51:06 +0200

    Declare year, month and day variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,15 @@
-var femaleNames =["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
-var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-var daysOfWeek = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
+var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 var gender = document.getElementsByName("gender").value;
+
+var calend = document.getElementById('bday').value;
+var birthday = calend.split("-");
+
+var year=parseInt(birthday[0]);
+var month=parseInt(birthday[1]);
+var day= parseInt(birthday[2]);
+var changeLabel = function () {
+    document.getElementById('result').innerHTML = "Kwame";
+}
+
