3f71bfb51cea8027babfc1af91d189c46d36340e
odetteahishakiye <ahiode6@gmail.com>
2019-07-19 12:28:11 +0200

    Variables declaration

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
 var femaleNames =["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 var daysOfWeek = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
+var gender = document.getElementsByName("gender").value;
+var 
